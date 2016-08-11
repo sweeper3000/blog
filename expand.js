b4bbb7383228c09d63251484d9249979611dd36e
sweeper3000 <spiketwofish@protonmail.com>
2016-08-10 20:52:06 -0400

    Post and add js and new css rules

---
+++ expand.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	// expand all posts
+	
+	$("#show-all").click(function(){
+		$(".post-content").show();
+	});
+
+	// expand "Use Signal"
+
+	$("#expand-signal").click(function(){
+		$("#signal-01").show();
+	});
+});
